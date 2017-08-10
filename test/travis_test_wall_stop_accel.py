85d6ead7f040ce8e6ea9adee369717d2689fd771
Kotaro Ashizawa <k-ashizawa@hykwlab.org>
2017-08-10 09:56:15 +0000

    p173 test32

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -19,7 +19,7 @@ class WallStopAccelTest(unittest.TestCase):
 
     def test_io(self):
 	left, right = self.set_and_get(400,100,100,0) #total: 600
-	self.assertTrue(left == 0 and right == 0,"can't stop")
+	self.assertTrue(left == right == 0,"can't stop")
 
 	left, right = self.set_and_get(40,0,0,9) #total: 49
 	self.assertTrue(left == right < 1000, "can't move again")
@@ -29,7 +29,7 @@ class WallStopAccelTest(unittest.TestCase):
 	self.assertTrue(2000 < left == right, "can't accerelate")
 
 	left, right = self.set_and_get(15,0,20,15) #total: 50
-	self.assertTrue(left == 0 and right == 0, "can't stop again")
+	self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
