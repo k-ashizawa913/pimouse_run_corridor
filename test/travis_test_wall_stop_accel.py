9d049b053033dd830de5a9674f81e3deea3f9b17
Kotaro Ashizawa <k-ashizawa@hykwlab.org>
2017-08-10 08:00:17 +0000

    p173 test4

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -34,4 +34,4 @@ class WallStopAccelTest(unittest.TestCase):
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_wall_stop_accel')
-    rostest.rosurun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopAccelTest)
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopAccelTest)
