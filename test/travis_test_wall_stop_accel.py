ddce80ca83e134c21e4445e434a72c4886e35301
Kotaro Ashizawa <k-ashizawa@hykwlab.org>
2017-08-10 07:52:59 +0000

    p173 test3

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -25,7 +25,7 @@ class WallStopAccelTest(unittest.TestCase):
 	self.assartTrue(left == right < 1000, "can't move again")
 
 	time.sleep(5.0)
-	left, right = self.set_and_get(40,0,0,9 #total: 49
+	left, right = self.set_and_get(40,0,0,9) #total: 49
 	self.assertTrue(2000 < left == right, "can't accerelate")
 
 	left, right = self.set_and_get(15,0,20,15) #total: 50
